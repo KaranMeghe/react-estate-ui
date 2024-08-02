a3fa4b52308b4d865a3d4d9d9ee4b68a55f0d852
Karan Meghe <karanmeghe87@gmail.com>
2024-08-03 00:38:01 +0530

    style - SearchBar component is responsive

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,60 @@
+import { useState } from "react";
 import "./searchbar.scss";
 
+const types = ["buy", "rent"];
+
 const SearchBar = () => {
-  return <div className="searchBar">SearchBar</div>;
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (value) => {
+    setQuery({ ...query, type: value });
+    console.log(query);
+  };
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => {
+          return (
+            <button
+              key={type}
+              onClick={() => switchType(type)}
+              className={query.type === type ? "active" : ""}
+            >
+              {type}
+            </button>
+          );
+        })}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={1000000000}
+          placeholder="Min Price"
+        />
+
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={1000000000}
+          placeholder="Max Price"
+        />
+
+        <button>
+          <img src="/search.png" alt="searchIcon" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -14,6 +14,23 @@ const HomePage = () => {
             amet consectetur adipisicing elit. Assumenda, tenetur.
           </p>
           <SearchBar />
+
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
         </div>
       </div>
       <div className="imgContainer">
